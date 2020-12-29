6115ee7cb62a020e4dcfe608cbaed06a0eaf571b
Abhi-1U <PerricoQ@outlook.com>
2020-12-29 23:32:16 +0530

    Design Changes

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2,6 +2,7 @@ function togglelight() {
     document.body.setAttribute('data-md-color-scheme','default');
     document.body.setAttribute('data-md-color-primary', 'teal');
     document.getElementsByTagName('meta')["theme-color"].content = "#009485";
+    document.querySelector("aside").style.backgroundColor='#009485';
     localStorage.setItem('themepref','default');
     localStorage.setItem('primary','white');
 }
@@ -9,6 +10,7 @@ function toggledark() {
     document.body.setAttribute('data-md-color-scheme', 'slate');
     document.body.setAttribute('data-md-color-primary', 'yellow');
     document.getElementsByTagName('meta')["theme-color"].content = "#ffec3d";
+    document.querySelector("aside").style.backgroundColor='#ffec3d';
     localStorage.setItem('themepref', 'slate');
     localStorage.setItem('primary','black');
 }
@@ -22,20 +24,26 @@ function switchTheme(e) {
 }
 var toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+
 function loadthemepreference() {
     var preference = localStorage.getItem('themepref');
     if (preference == null) {
         localStorage.setItem('themepref', 'default');
+        document.querySelector("aside").style.backgroundColor='#009485';
         document.body.setAttribute('data-md-color-scheme', "default");
         document.body.setAttribute('data-md-color-primary', 'teal');
         document.getElementsByTagName('meta')["theme-color"].content = "#009485";
+        
     }
     else {
         if(preference === "slate"){
             toggleSwitch.checked = true;
+            document.querySelector("aside").style.backgroundColor='#ffec3d';
             document.body.setAttribute('data-md-color-scheme', preference);
             document.body.setAttribute('data-md-color-primary', 'yellow');
             document.getElementsByTagName('meta')["theme-color"].content = "#ffec3d";
+            
         }
     }
 }
